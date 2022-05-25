be13d3bfb6168e0d30fe5a260c9f2f3a2625f4c8
TheNam74 <nguyenthenam1910@gmail.com>
2022-05-25 23:23:22 +0700

    init

---
+++ TKPM/TKPM/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TKPM.Models;
+
+namespace TKPM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ThemDaiLy()
+        {
+            return View();
+        }
+        public IActionResult DanhSachDaiLy()
+        {
+            return View();
+        }
+        public IActionResult ChiTietDaiLy()
+        {
+            return View();
+        }
+        public IActionResult LichSuXuatHang ()
+        {
+            return View();
+        }
+        public IActionResult LapPhieuXuatHang()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
