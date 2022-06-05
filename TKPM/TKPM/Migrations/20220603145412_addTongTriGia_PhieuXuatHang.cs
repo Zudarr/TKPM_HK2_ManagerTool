d479276b1f9cf8fa483a13c1d07d080d5e15e958
TheNam74 <nguyenthenam1910@gmail.com>
2022-06-05 23:24:08 +0700

    change connection string

---
+++ TKPM/TKPM/Migrations/20220603145412_addTongTriGia_PhieuXuatHang.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TKPM.Migrations
+{
+    public partial class addTongTriGia_PhieuXuatHang : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TongTriGia",
+                table: "PhieuXuatHangs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TongTriGia",
+                table: "PhieuXuatHangs");
+        }
+    }
+}
---
+++ TKPM/TKPM/Models/ViewModels/BaoCaoDoanhThuVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TKPM.Models
+{
+    public class BaoCaoDoanhThuVM
+    {
+        public int Thang { get; set; }
+        //List<ChiTietXuatHang>
+    }
+}
