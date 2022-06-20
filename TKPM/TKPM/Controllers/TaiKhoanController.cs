e7bf37b21ef8432de20185630dd83062026ff7b4
trandactoan <73386087+trandactoan@users.noreply.github.com>
2022-06-20 23:10:20 +0700

    Add TaiKhoan function and authorize
    ...

---
+++ TKPM/TKPM/Controllers/TaiKhoanController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TKPM.Data;
+using TKPM.Models;
+namespace TKPM.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TaiKhoanController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return RedirectToAction("DanhSachTaiKhoan");
+        }
+        public IActionResult DanhSachTaiKhoan()
+        {
+            IQueryable<ApplicationUser> danhSachTaiKhoan = _db.TaiKhoans;
+            return View("DanhSachTaiKhoan", danhSachTaiKhoan.ToList());
+        }
+        public IActionResult XoaTaiKhoan(string? id)
+        {
+            ApplicationUser TaiKhoanXoa = _db.TaiKhoans.FirstOrDefault(x => x.Id == id);
+            _db.TaiKhoans.Remove(TaiKhoanXoa);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
